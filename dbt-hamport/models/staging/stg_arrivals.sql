7b025db6df135ce46406e72700ef7ab9385b8031
Melvin Speckamp <doctype.melvin@gmail.com>
2026-04-22 12:21:48 +0200

    handle future state and formatting

--- dbt-hamport/models/staging/stg_arrivals.sql
+++ dbt-hamport/models/staging/stg_arrivals.sql
@@ -18,14 +18,19 @@ final as (
         *,
         case
             when cancelled is true then null
-            when actual_time is null then 0
-            else extract(epoch from (planned_time - actual_time)) / 60
-            end as delay_minutes,
+            when actual_time is null and planned_time > now() then 0 -- future scheduled flight
+            when actual_time is null and planned_time <= now() then -- delayed flight not landed yet
+                extract(epoch from (now() - planned_time)) / 60
+            else extract(epoch from (actual_time - planned_time)) / 60 -- flight arrived
+        end as delay_minutes,
         
         case
             when cancelled is true then 'Cancelled'
-            when actual_time is null then 'In-Flight'
+            when actual_time is null and planned_time > now() then 'Scheduled'
+            when actual_time is null and planned_time <= now() then 'En Route / Delayed'
             else 'Arrived'
+        end as flight_status
+        
     from cleaned
 )
 
