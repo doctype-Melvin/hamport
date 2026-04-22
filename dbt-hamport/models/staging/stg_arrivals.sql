d9c3a4c0844cfc293b539f8955e55edd9ddccdac
Melvin Speckamp <doctype.melvin@gmail.com>
2026-04-22 12:14:33 +0200

    add tests and handle cancelled flights

--- dbt-hamport/models/staging/stg_arrivals.sql
+++ dbt-hamport/models/staging/stg_arrivals.sql
@@ -2,14 +2,31 @@ with source as (
     select * from {{ source('raw_data', 'raw_arrivals')}}
 ),
 
-renamed as (
+cleaned as (
     select
         "flightnumber"::varchar as flight_id,
         "airlineName"::varchar as airline,
-        "plannedArrivalTime"::varchar as planned_time,
-        "expectedArrivalTime"::varchar as actual_time,
-        "originAirportLongName"::varchar as origin_airport
+        "originAirportLongName"::varchar as origin_airport,
+        cancelled,
+        regexp_replace("plannedArrivalTime", '\[.*\]', '')::timestamptz as planned_time,
+        regexp_replace("expectedArrivalTime", '\[.*\]', '')::timestamptz as actual_time
     from source
+),
+
+final as (
+    select 
+        *,
+        case
+            when cancelled is true then null
+            when actual_time is null then 0
+            else extract(epoch from (planned_time - actual_time)) / 60
+            end as delay_minutes,
+        
+        case
+            when cancelled is true then 'Cancelled'
+            when actual_time is null then 'In-Flight'
+            else 'Arrived'
+    from cleaned
 )
 
-select * from renamed
+select * from final
