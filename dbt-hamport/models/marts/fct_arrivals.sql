57c74033d480759abdb1c047bb9da4c64e22bc65
Melvin Speckamp <doctype.melvin@gmail.com>
2026-04-23 07:40:05 +0200

    add planned hour and day columns

--- dbt-hamport/models/marts/fct_arrivals.sql
+++ dbt-hamport/models/marts/fct_arrivals.sql
@@ -3,3 +3,19 @@
 with staging as (
     select * from {{ref('stg_arrivals')}}
 )
+
+select
+    md5(concat(flight_id, planned_time::text)) as arrival_key, -- unique identifier
+    flight_id,
+    airline,
+    origin_airport,
+    planned_time,
+    actual_time,
+    delay_minutes,
+    flight_status,
+
+    -- enrichment from temporal data
+    extract(hour from planned_time) as planned_hour,
+    to_char(planned_time, 'Day') as day_of_week
+
+from staging
