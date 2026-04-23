9c1778d74a20ec4d3807eff2db04cface7d2ca04
Melvin Speckamp <doctype.melvin@gmail.com>
2026-04-23 09:34:16 +0200

    update fetch function to call both endpoints

--- extract_flights.py
+++ extract_flights.py
@@ -2,7 +2,7 @@ import requests
 import os
 import pandas as pd
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.dialects.postgresql import JSONB
 
 load_dotenv()
@@ -10,16 +10,17 @@ load_dotenv()
 # Credentials config
 API_KEY = os.getenv("HAM_API_KEY")
 SUPABASE = os.getenv("SUPA_URL")
-HAM_API_URL = "https://rest.api.hamburg-airport.de/v2/flights/arrivals"
+
 HEADERS = {
     "Ocp-Apim-Subscription-Key": API_KEY
 }
 DB_URL = SUPABASE
 
-def fetch_and_load():
+def fetch_and_load(endpoint):
+    URL = f"https://rest.api.hamburg-airport.de/v2/flights/{endpoint}"
     try:
-        print("Attempting fetch from HAM API...")
-        response = requests.get(HAM_API_URL, headers=HEADERS, timeout=10)
+        print(f"Attempting fetch {endpoint} from HAM API...")
+        response = requests.get(URL, headers=HEADERS, timeout=10)
         response.raise_for_status()
         data = response.json()
 
@@ -30,17 +31,21 @@ def fetch_and_load():
         df = pd.DataFrame(data)
         print(f"Success: Fetched {len(df)} records")
 
+        table_name = f"raw_{endpoint}"
         engine = create_engine(DB_URL)
 
+        with engine.begin() as connection:
+            connection.execute(text(f"TRUNCATE TABLE raw.{table_name}"))
+
         df.to_sql(
-            'raw_arrivals',
+            table_name,
             engine,
             schema='raw',
-            if_exists='replace',
+            if_exists='append',
             index=False
         )
 
-        print("Success: Data loaded to schema 'raw'")
+        print(f"Success: Data loaded to schema {table_name}")
     
     except requests.exceptions.HTTPError as errh:
         print(f"Fail: HTTP Error: {errh}")
@@ -52,4 +57,5 @@ def fetch_and_load():
         print(f"Fail: Unexpected Error: {e}")
 
 if __name__ == "__main__":
-    fetch_and_load()
+    for direction in ['arrivals', 'departures']:
+        fetch_and_load(direction)
