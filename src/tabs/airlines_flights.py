bc34850dfe509115894768601480ed3b3ee07a3e
Melvin Speckamp <doctype.melvin@gmail.com>
2026-06-10 10:40:08 +0200

    add metric to airlines tab

--- src/tabs/airlines_flights.py
+++ src/tabs/airlines_flights.py
@@ -2,29 +2,18 @@ import streamlit as st
 
 def render_airlines(data):
     data_top5_airlines = data["airlines_stats"].sort_values('number_of_flights', ascending=False).head(5)
-    data_busy_hours = data["airport_stats"].sort_values('avg_hourly_flights', ascending=False).head(5)
 
-    col1, col2 = st.columns(2)
-    with col1:
-        st.subheader('Airlines with the most flights')
-        st.bar_chart(
-            data=data_top5_airlines,
-            x='airline_group',
-            x_label='Airline Group',
-            y='number_of_flights',
-            y_label='Flights',
-            horizontal=True,
-            sort='-number_of_flights'
-        )
+    st.metric(label='Active Airlines', value=len(data["airlines_stats"]), border=True, width='content')
 
-    with col2:
-        st.subheader('Busy hours at Hamburg Airport')
-        st.bar_chart(
-            data=data_busy_hours,
-            x='planned_hour',
-            x_label='Hour of Day',
-            y='avg_hourly_flights',
-            y_label='Flights',
-            horizontal=True,
-            sort='-avg_hourly_flights'
-        )
+    st.subheader('Top 5 Airlines by flight volume', text_alignment='center')
+    st.bar_chart(
+        data=data_top5_airlines,
+        x='airline_group',
+        x_label='Airline Group',
+        y='number_of_flights',
+        y_label='Flights',
+        horizontal=True,
+        sort='-number_of_flights'
+    )
+
+ 
