a3feab590f82be024087986c400b25489db27c5d
Melvin Speckamp <doctype.melvin@gmail.com>
2026-06-10 09:56:10 +0200

    add container to overview tab

--- src/tabs/overview.py
+++ src/tabs/overview.py
@@ -11,13 +11,29 @@ def render_overview(data):
         y='flight_count',
         y_label='Count'
     )
-    
-    st.subheader('Busy hours at Hamburg Airport')
-    st.scatter_chart(
-        data=data_busy_hours,
-        x='planned_hour',
-        x_label='Hour of Day',
-        y='avg_hourly_flights',
-        y_label='Flights',
-        color='direction'
-    )
+
+    col1, col2, col3 = st.columns([0.5, 0.1, 0.5])
+
+    with col1:
+        st.subheader('Busy hours at Hamburg Airport', text_alignment='center')
+        st.scatter_chart(
+            data=data_busy_hours,
+            x='planned_hour',
+            x_label='Hour of Day',
+            y='avg_hourly_flights',
+            y_label='Flights',
+            size='avg_hourly_flights',
+            color='direction'
+        )
+
+    with col3:
+        st.subheader('Average delays by hour', text_alignment='center')
+        st.bar_chart(
+            data=data["airport_stats"],
+            x='planned_hour',
+            x_label='Hour of Day',
+            y='avg_hourly_delay',
+            y_label='Delay in minutes',
+            stack=False,
+            color='direction',
+        )
